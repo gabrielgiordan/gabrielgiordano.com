99a33700b314313598a4a210b137bf76b7ca4ff2
Gabriel Medina <hello@gabrielgiordano.com>
2020-06-25 18:15:22 -0300

    Add dns preconnect Also, add analytics and display swap web fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,27 +1,28 @@
 module.exports = {
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-react-helmet",
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: `images`,
+        name: "images",
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: "gatsby-plugin-manifest",
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#222222`,
-        theme_color: `#222222`,
-        display: `minimal-ui`,
-        icon: `src/images/icon.png`,
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#222222",
+        theme_color: "#222222",
+        display: "minimal-ui",
+        icon: "src/images/icon.png",
       },
     },
+    "gatsby-plugin-optimize-svgs",
     {
       resolve: "gatsby-plugin-react-svg",
       options: {
@@ -30,30 +31,35 @@ module.exports = {
         },
       },
     },
-    `gatsby-plugin-optimize-svgs`,
-    `gatsby-plugin-styled-components`,
+    "gatsby-plugin-styled-components",
     {
-      resolve: "gatsby-plugin-web-font-loader",
+      resolve: "gatsby-source-filesystem",
       options: {
-        google: {
-          families: ["Open Sans:400,700:latin", "sans-serif"],
-        },
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown`,
+        name: "markdown",
         path: `${__dirname}/src/markdown`,
       },
     },
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-offline`,
+    "gatsby-transformer-remark",
     {
-      resolve: `gatsby-plugin-gtag`,
+      resolve: "gatsby-plugin-gtag",
       options: {
         trackingId: "UA-48045203-1",
       },
     },
+    {
+      resolve: "gatsby-plugin-web-font-loader",
+      options: {
+        google: {
+          families: ["Open Sans:400,700:latin&display=swap", "sans-serif"],
+        },
+      },
+    },
+    {
+      resolve: "gatsby-plugin-preconnect",
+      options: {
+        domains: ["https://fonts.gstatic.com", "https://fonts.googleapis.com"],
+      },
+    },
+    "gatsby-plugin-offline"
   ],
 };
